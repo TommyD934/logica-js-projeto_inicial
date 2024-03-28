bf1a22c12d87a5e57b79f4a8c34a7f455693128b
TommyD934 <165021301+TommyD934@users.noreply.github.com>
2024-03-28 07:42:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ alert("bem vindo ao jogo");
 
 let numeroSecreto = Math.floor(Math.random() * 51);
 
-let numeroTentativas = 0;
+let Tentativas= 6;
 
 
-while (numeroTentativas <= 5) {
+while (Tentativas > 0 ) {
     let numeroEscolhido = parseInt(prompt(`digite um numero de 0 a 50`));
     if (numeroSecreto == numeroEscolhido) {
         alert(`parabens voce acertou o numero ${numeroEscolhido}`);
         break;
     } else if (numeroEscolhido > numeroSecreto) {
-        alert(`voce digitou ${numeroEscolhido} é maior que o númeo secreto`);
+        alert(`voce digitou um numero maior que o númeo secreto`);
     } else {
-        alert(`voce digitou ${numeroEscolhido} é menor que o numero Secreto`);
+        alert(`voce digitou um numero  menor que o numero Secreto`);
     };
-    numeroTentativas = numeroTentativas + 1;
+    alert(`você ainda tem  ${Tentativas} Tentativas `)
+    Tentativas = Tentativas - 1;
 }
