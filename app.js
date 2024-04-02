dc044d0ad691a0f4c92608d128c45f34c1233286
TommyD934 <165021301+TommyD934@users.noreply.github.com>
2024-04-02 11:24:58 -0300

    e46r5k68m

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-alert("bem vindo ao jogo");
 
-let numeroSecreto = Math.floor(Math.random() * 51);
+function iniciar() {
+    alert("bem vindo ao jogo");
+    let numerodealeatorizar = parseInt(prompt('quamtos numeros você quer aleatorizar'))
 
-let Tentativas= 6;
+    function numeroAnf(index) {
+        return Math.floor(Math.random() * index + 1);
 
+    }
+    let numeroescoooo = numeroAnf(numerodealeatorizar)
 
-while (Tentativas > 0 ) {
-    let numeroEscolhido = parseInt(prompt(`digite um numero de 0 a 50`));
-    if (numeroSecreto == numeroEscolhido) {
-        alert(`parabens voce acertou o numero ${numeroEscolhido}`);
-        break;
-    } else if (numeroEscolhido > numeroSecreto) {
-        alert(`voce digitou um numero maior que o númeo secreto`);
-    } else {
-        alert(`voce digitou um numero  menor que o numero Secreto`);
-    };
-    alert(`você ainda tem  ${Tentativas} Tentativas `)
-    Tentativas = Tentativas - 1;
-}
-    if (Tentativas === 4) {
+    for (let Tentativas = 0; Tentativas < numerodetentativas;  Tentativas++) {
+
+        let numeroEscolhido = parseInt(prompt(`digite um numero de 1 a ${numerodealeatorizar}`));
+
+        if (numeroescoooo === numeroEscolhido){
+            
+            alert(`parabens voce acertou o numero ${numeroEscolhido}`);
+            break;
+        } else if (numeroEscolhido > numeroescoooo) {
+            alert(`voce digitou um maior que o númeo secreto`);
+        } else {
+            alert(`voce digitou um menor que o numero Secreto`);
+        };
+        alert(`você ainda tem  ${Tentativas} Tentativas `)
+    }
+    if (Tentativas === numerodetentativas) {
         alert(`Suas tentativas acabaram`);
-    }else {
-        alert(`você ainda tem ${4 - Tentativas} tentativas`)
-}
-let palavraTentativa = Tentativas > 1 ? `Tentativas` : `Tentativas`
-if (Tentativas < 4) {
-    alert(`você descobriu o número secreto em ${Tentativas} ${palavraTentativa}`)
+         
+    } else if (Tentativas <= numerodetentativas) {
 
-}
+        alert(`você ainda tem ${numerodetentativas - Tentativas - 1 } tentativas`);
 
-for(let Tentativas = 0; tentativas <6; Tentativas++){}
+    } else {
+
+        (`você ainda tem ${numerodetentativas - Tentativas - 1 } Tentativas`);
+        let numerodetentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        if(tentativas < 4){
+            alert(`Você acertou com ${Tentativas} ${numerodetentativas}`)
+    }
+  }
+}
